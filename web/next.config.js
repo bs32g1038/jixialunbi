7b557f7a33e816be6bc3e862f9efbd9b0816cebf
bs32g1038 <bs32g1038@163.com>
2022-11-06 19:18:28 +0800

    chore: adjust  config

--- web/next.config.js
+++ web/next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
+const proxy_host = process.env.PROXY_HOST || '127.0.0.1'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() { 
-    return [ 
-     //接口请求 前缀带上/api-text/
-      { source: '/api/:path*', destination: `http://127.0.0.1:4000/api/:path*` }, 
-      { source: '/static/:path*', destination: `http://127.0.0.1:4000/static/:path*` }, 
+  async rewrites() {
+    return [
+      //接口请求 前缀带上/api-text/
+      { source: '/api/:path*', destination: `http://${proxy_host}:4000/api/:path*` },
+      { source: '/static/:path*', destination: `http://${proxy_host}:4000/static/:path*` },
     ]
   },
   webpack: config => {
