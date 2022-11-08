2105456807dd05998758a4aa41601ae93e4e7b7f
bs32g1038 <bs32g1038@163.com>
2022-11-08 11:40:45 +0800

    fix: hidden preview text show

--- web/components/home/components/TopicItem/components/CImage/index.tsx
+++ web/components/home/components/TopicItem/components/CImage/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Image } from 'antd';
+import style from './index.module.scss';
 
 export default function CImage(props) {
     const { src } = props;
-    return <Image src={src} alt="" onLoad={(res: any) => {
+    return <Image rootClassName={style.wrap} src={src} alt="" onLoad={(res: any) => {
         const oImg = new window.Image();
         oImg.src = res.target.src;
         if (oImg.height / oImg.width >= 1.2) {
